a6194c2560715af00555ec5b6faa79765e38e52d
hamidreza <hamidreza.safayee@gmail.com>
2022-07-04 11:19:50 +0430

    Categories crud and test

--- backend/teachCode/database/seeders/DatabaseSeeder.php
+++ backend/teachCode/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Category::factory()->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- backend/teachCode/routes/api.php
+++ backend/teachCode/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+});
+
+Route::prefix('v1')->group(function () {
+    // 
 });
